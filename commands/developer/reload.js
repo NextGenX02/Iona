67e265690435e534a6277e26eba16c1df775ed76
Kiyzan <mzpnzakky@gmail.com>
2023-06-13 17:18:21 +0700

    Fix locale

--- commands/developer/reload.js
+++ commands/developer/reload.js
@@ -1,55 +1,40 @@
-const fs = require("fs")
+const { readdirSync } = require("node:fs")
 
 module.exports = {
-	name: "reload",
-	description: "Reloads a command",
-    category: "⚙️ Developer",
-    aliases: "r",
-    usage: "<command>",
+    aliases: [],
 	args: true,
-	devOnly: true,
+	category: "developer",
+    description: "",
+	developerOnly: false,
+	guildOnly: false,
+    name: "reload",
+    usage: "<command>",
 
-	execute(message, args) {
+    execute(message, args) {
 
 		const commandName = args[0].toLowerCase()
-
-		const command =
-			message.client.commands.get(commandName) ||
-			message.client.commands.find(
-				(cmd) => cmd.aliases && cmd.aliases.includes(commandName)
-			)
+		const command = message.client.commands.get(commandName) || message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName))
 
 		if (!command) {
-			return message.channel.send({
-				content: `There is no command with name or alias \`${commandName}\`, ${message.author}!`,
+			return message.reply({
+				content: i18n.__("commands.developer.reload.not_command") + commandName + "!"
 			})
 		}
 
-		const commandFolders = fs.readdirSync("./commands")
+		commandFolder = readdirSync("./commands")
+		const folderName = commandFolder.find(folder => readdirSync(`./commands/${folder}`).includes(`${command.name}.js`))
 
-		const folderName = commandFolders.find((folder) =>
-			fs.readdirSync(`./commands/${folder}`).includes(`${command.name}.js`)
-		)
-
-		delete require.cache[
-			require.resolve(`../${folderName}/${command.name}.js`)
-		]
+		delete require.cache[require.resolve(`../${folderName}/${command.name}.js`)]
 
 		try {
 
 			const newCommand = require(`../${folderName}/${command.name}.js`)
 
 			message.client.commands.set(newCommand.name, newCommand)
-
-			message.channel.send({
-				content: `Command \`${newCommand.name}\` was reloaded!`,
-			})
+			message.channel.send({ content: i18n.__mf("commands.developer.reload.error", { command: newCommand.name }), })
 		} catch (error) {
-
-			console.error(error);
-			message.channel.send({
-				content: `There was an error while reloading a command \`${command.name}\`:\n\`${error.message}\``,
-			})
+			console.error(error)
+			message.channel.send({ content: i18n.__("commands.developer.reload.error") + `\`${command.name}\`:\n\`${error.message}\``, })
 		}
-	},
+	}
 }
