a0256b5a3fb4209769171d5fd49ea24d55d90e41
Kiyzan <mzpnzakky@gmail.com>
2023-06-14 12:44:24 +0700

    New cmd: clear,js

---
+++ commands/admin/clear.js
@@ -0,0 +1,29 @@
+
+const i18n = require("i18n")
+
+module.exports = {
+    aliases: ["bulk", "delete"],
+	args: true,
+	category: "admin",
+    description: "Delete all messages that don't need again",
+	developerOnly: false,
+	guildOnly: true,
+    name: "clear",
+    usage: "<numbers>",
+
+    execute(message, args) {
+
+        if (isNaN(args[0])) {
+            return message.channel.sendTyping().then(message.reply(i18n.__("commands.admin.delete_messages.uknown")))
+        }
+
+        if (args[0] <= 100) {
+            
+            message.channel.bulkDelete(args[0])
+                .then(messages => message.channel.send(i18n.__mf("commands.admin.delete_messages.total_delete", { size: messages.size })))
+        } else {
+
+            message.channel.sendTyping().then(message.reply(i18n.__mf("commands.admin.delete_messages.fail", { total: args[0] })))
+        }
+    }
+}
